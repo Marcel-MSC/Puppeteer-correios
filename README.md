a1e8c34e61749c759faedc7be24c6d315e00835e
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2024-04-26 13:41:35 -0300

    Change index to work on current Correios website, changed readme, add git ignore file

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
-Experimentando o puppeteer com o site do correios.
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=200px height=200px src="./images/puppeteer.png" alt="Project logo"></a>
+</p>
 
-Trying out puppeteer with the post office website.
+<h3 align="center">puppeteer-correios</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](/LICENSE)
+
+</div>
+
+---
+
+<p align="center"> It's a simple use of Puppeter to get a photo of a giving CEP.<br> </p>
+
+## 📝 Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Authors](#authors)
+
+## 🧐 About <a name = "about"></a>
+
+Open a headless browser open a URL to Correios and take a photo of the address found.
+
+## 🏁 Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+Change the second param of the type function to a valid CEP to search.
+
+```
+await page.type('#endereco', '03336000');
+```
+
+### Installing
+
+```
+npm install
+```
+
+```
+npm run start
+```
+
+## 🎈 Usage <a name="usage"></a>
+Search a valid CEP string and take a photo of the result.
+
+## ✍️ Authors <a name = "authors"></a>
+
+- [@Marcel-MSC](https://github.com/Marcel-MSC) - Idea & Initial work
--- index.js
+++ index.js
@@ -1,56 +1,64 @@
 const Puppeteer = require('puppeteer')
 
-;(async() => {
-    const browser = await Puppeteer.launch({
-        headless: true,
-        //defaultViewport: null,
-        //executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
-        //slowMo: 500,
-        //args: [ '--start-maximized']
-    })
-    const page = await browser.newPage()
-    await page.goto("https://www.correios.com.br")
-    await page.waitForSelector('#relaxation')
-    await page.type('#relaxation', '03336000')
-    const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page() )))
-    await page.keyboard.press('Enter')
-    const page2 = await newPagePromise;
-    await page2.bringToFront(); 
+    ; (async () => {
+        const browser = await Puppeteer.launch({
+            headless: true,
+            //defaultViewport: null,
+            //executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+            //slowMo: 500,
+            //args: [ '--start-maximized']
+        })
+        const page = await browser.newPage()
+        //await page.goto("https://www.correios.com.br")
+        await page.goto("https://buscacepinter.correios.com.br/app/endereco/index.php");
+        await page.waitForSelector('#endereco');
+        await page.type('#endereco', '03336000');
+        await page.click("#btn_pesquisar");
+        await page.waitForSelector("#btn_nbusca");
 
-    await page2.waitForResponse(response => response.url().includes('/app/endereco/'));
-    
-    let headerData = await page2.$$eval('table thead tr th', ths => ths.map( (th) => {
-        return th.innerText;
-    })) 
-    console.log('headerData: ', headerData)
+        await page.screenshot({ path: './images/correios.png' });
+
+        // Implementar novamente com as mudanças de ids, table enfim tudo
+
+        //const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page() )))
+        //await page.keyboard.press('Enter')
+        //const page2 = await newPagePromise;
+        //await page2.bringToFront(); 
+
+        //await page2.waitForResponse(response => response.url().includes('/app/endereco/'));
+
+        // let headerData = await page2.$$eval('table thead tr th', ths => ths.map( (th) => {
+        //     return th.innerText;
+        // })) 
+        // console.log('headerData: ', headerData)
 
 
-    let bodyData = await page2.$$eval('table tbody tr td', tds => tds.map( (td) => {
-        return td.innerText;
-    }))
-    console.log('bodyData: ', bodyData)
+        // let bodyData = await page2.$$eval('table tbody tr td', tds => tds.map( (td) => {
+        //     return td.innerText;
+        // }))
+        // console.log('bodyData: ', bodyData)
 
-    let fullData = [];
-    fullData.push(headerData)
-    fullData.push(bodyData)
-    console.log('fullData', fullData)
+        // let fullData = [];
+        // fullData.push(headerData)
+        // fullData.push(bodyData)
+        // console.log('fullData', fullData)
 
-    let obj = {};
-    for (let i = 0; i < headerData.length; i++) {
-        obj[headerData[i]] = bodyData[i];
-    }
-    console.log('objectData', obj)
+        // let obj = {};
+        // for (let i = 0; i < headerData.length; i++) {
+        //     obj[headerData[i]] = bodyData[i];
+        // }
+        // console.log('objectData', obj)
 
-    let mapObj = new Map(); 
-    for(let i = 0; i < headerData.length; i++){ 
-        mapObj.set(headerData[i], bodyData[i]); 
-    }
-    console.log('mapObj', mapObj)
+        // let mapObj = new Map(); 
+        // for(let i = 0; i < headerData.length; i++){ 
+        //     mapObj.set(headerData[i], bodyData[i]); 
+        // }
+        // console.log('mapObj', mapObj)
 
-    let table = await page2.$eval('#resultado-DNEC', (el) => el.outerHTML)
-    console.log('Table: ', table)
-    
-    await page2.screenshot({ path: 'correios.png' });
+        // let table = await page2.$eval('#resultado-DNEC', (el) => el.outerHTML)
+        // console.log('Table: ', table)
 
-    await browser.close()
-})()
+        //await page.screenshot({ path: 'correios.png' });
+
+        await browser.close()
+    })()
